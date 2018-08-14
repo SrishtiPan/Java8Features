a54e50c218270f33595542fc1954d4558ac896ba
SrishtiPan <ginnypant2005@gmail.com.com>
2018-08-14 11:20:53 +0530

    committed

---
+++ src/incubation/DoubleCheckedLocking.java
@@ -0,0 +1,18 @@
+package incubation;
+
+
+public class DoubleCheckedLocking {
+    private static volatile DoubleCheckedLocking instance;
+    public static DoubleCheckedLocking getInstance() {
+        if (instance == null) {
+            synchronized (DoubleCheckedLocking .class) {
+                if (instance == null) {
+                    instance = new DoubleCheckedLocking();
+                }
+            }
+        }
+        return instance;
+    }
+ 
+    private DoubleCheckedLocking(){}
+}
---
+++ src/incubation/SingletonLazyInitialization.java
@@ -0,0 +1,16 @@
+package incubation;
+
+
+public class SingletonLazyInitialization {
+
+    private static SingletonLazyInitialization instance;
+    
+    private SingletonLazyInitialization(){}
+    
+    public static SingletonLazyInitialization getInstance(){
+        if(instance == null){
+            instance = new SingletonLazyInitialization();
+        }
+        return instance;
+    }
+}
